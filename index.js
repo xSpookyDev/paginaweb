88ff469df98c022a34d78f282b92e2568b1f006f
unknown <ricardo13dev@gmail.com>
2025-07-26 22:35:35 -0400

    Agregando efectos en hero section

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // initialization
-
 const RESPONSIVE_WIDTH = 1024
 
 let headerWhiteBg = false
@@ -13,14 +12,11 @@ const navDropdown = document.querySelector("#nav-dropdown-list-0")
 const videoBg = document.querySelector("#video-container-bg")
 const videoContainer = document.querySelector("#video-container")
 
-function onHeaderClickOutside(e) {
-
+const onHeaderClickOutside = (e)=> {
     if (!collapseHeaderItems.contains(e.target)) {
         toggleHeader()
     }
-
 }
-
 
 function toggleHeader() {
     if (isHeaderCollapsed) {
@@ -104,78 +100,7 @@ function updateToggleModeBtn(){
 }
 
 
-const promptWindow =  new Prompt("#pixa-playground")
-const promptForm = document.querySelector("#prompt-form")
-const promptInput = promptForm.querySelector("input[name='prompt']")
 
-const MAX_PROMPTS = 3
-
-promptForm.addEventListener("submit", (event) => {
-    event.preventDefault()
-
-    // window.open("https://github.com/PaulleDemon", "_blank")
-
-    if (promptWindow.promptList.length >= MAX_PROMPTS)
-        return false
-
-    promptWindow.addPrompt(promptInput.value)
-    promptInput.value = ""
-    
-    if (promptWindow.promptList.length >= MAX_PROMPTS){
-        // prompt signup once the user makes 3 prompts, ideally must be throttled via backend API
-        const signUpPrompt = document.querySelector("#signup-prompt")
-        signUpPrompt.classList.add("tw-scale-100")
-        signUpPrompt.classList.remove("tw-scale-0")
-
-        promptForm.querySelectorAll("input").forEach(e => {e.disabled = true})
-    }
-
-    return false
-})
-
-const dropdowns = document.querySelectorAll('.dropdown')
-dropdowns.forEach(dropdown => new Dropdown(`#${dropdown.id}`, promptWindow.setAIModel))
-
-
-navToggle.addEventListener("click", toggleNavDropdown)
-navDropdown.addEventListener("mouseleave", closeNavDropdown)
-
-function toggleNavDropdown(){
-
-    if (navDropdown.getAttribute("data-open") === "true"){
-        closeNavDropdown()
-    }else{
-        openNavDropdown()
-    }
-}
-
-function navMouseLeave(){
-    setTimeout(closeNavDropdown, 100)
-}
-
-function openNavDropdown(event){
-
-    navDropdown.classList.add("tw-opacity-100", "tw-scale-100", 
-                            "max-lg:tw-min-h-[450px]", "max-lg:!tw-h-fit", "tw-min-w-[320px]")
-    
-    navDropdown.setAttribute("data-open", true)
-
-}
-
-function closeNavDropdown(event){
-
-    // console.log("event target: ", event.target, event.target.contains(navDropdown))
-    
-    if (navDropdown.matches(":hover")){
-        return
-    }
-
-    navDropdown.classList.remove("tw-opacity-100", "tw-scale-100", 
-        "max-lg:tw-min-h-[450px]", "tw-min-w-[320px]", "max-lg:!tw-h-fit",)
-
-    navDropdown.setAttribute("data-open", false)
-
-}
 
 function openVideo(){
     videoBg.classList.remove("tw-scale-0", "tw-opacity-0")
@@ -204,90 +129,11 @@ function closeVideo(){
  * Animations
  */
 
-const typed = new Typed('#prompts-sample', {
-    strings: ["How to solve a rubik's cube? Step by step guide", 
-                "What's Pixa playground?", 
-                "How to build an AI SaaS App?", 
-                "How to integrate Pixa API?"],
-    typeSpeed: 80,
-    smartBackspace: true, 
-    loop: true,
-    backDelay: 2000,
-})
-
-gsap.registerPlugin(ScrollTrigger)
 
 
-gsap.to(".reveal-up", {
-    opacity: 0,
-    y: "100%",
-})
 
-
-// straightens the slanting image
-gsap.to("#dashboard", {
-
-    scale: 1,
-    translateY: 0,
-    // translateY: "0%",
-    rotateX: "0deg",
-    scrollTrigger: {
-        trigger: "#hero-section",
-        start: window.innerWidth > RESPONSIVE_WIDTH ? "top 95%" : "top 70%",
-        end: "bottom bottom",
-        scrub: 1,
-        // markers: true,
-    }
-
-})
 
 const faqAccordion = document.querySelectorAll('.faq-accordion')
 
-faqAccordion.forEach(function (btn) {
-    btn.addEventListener('click', function () {
-        this.classList.toggle('active')
-
-        // Toggle 'rotate' class to rotate the arrow
-        let content = this.nextElementSibling
-        let icon = this.querySelector(".bi-plus")
-
-        // content.classList.toggle('!tw-hidden')
-        if (content.style.maxHeight === '240px') {
-            content.style.maxHeight = '0px'
-            content.style.padding = '0px 18px'
-            icon.style.transform = "rotate(0deg)"
-            
-        } else {
-            content.style.maxHeight = '240px'
-            content.style.padding = '20px 18px'
-            icon.style.transform = "rotate(45deg)"
-        }
-    })
-})
 
 
-
-// ------------- reveal section animations ---------------
-
-const sections = gsap.utils.toArray("section")
-
-sections.forEach((sec) => {
-
-    const revealUptimeline = gsap.timeline({paused: true, 
-                                            scrollTrigger: {
-                                                            trigger: sec,
-                                                            start: "10% 80%", // top of trigger hits the top of viewport
-                                                            end: "20% 90%",
-                                                            // markers: true,
-                                                            // scrub: 1,
-                                                        }})
-
-    revealUptimeline.to(sec.querySelectorAll(".reveal-up"), {
-        opacity: 1,
-        duration: 0.8,
-        y: "0%",
-        stagger: 0.2,
-    })
-
-
-})
